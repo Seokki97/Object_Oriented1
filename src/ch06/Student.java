b205c1e6a820dd9b4f38b50b076fb160cb912af1
programmers1997 <skaksdl1238@gmail.com>
2022-03-12 11:52:09 +0900

    Commit

--- src/ch06/Student.java
+++ src/ch06/Student.java
@@ -6,6 +6,7 @@ public class Student {
     public String studentName;
 
     public String showStudentInfo(){
+
         return studentNumber +"은" + grade + "학녀이고" + "이름은" + studentName +"입니다.";
     }
 }
