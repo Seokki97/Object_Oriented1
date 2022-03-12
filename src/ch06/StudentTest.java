2ed6a7bd5bd0d173c46cc7266455d7cb0f14f630
programmers1997 <skaksdl1238@gmail.com>
2022-03-12 12:14:56 +0900

    this 생성자

--- src/ch06/StudentTest.java
+++ src/ch06/StudentTest.java
@@ -2,6 +2,7 @@ package ch06;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student StudentLee = new Student();
+        Student StudentLee = new Student(1, 2, "leet-shirt");
+        System.out.println(StudentLee.showStudentInfo());
     }
 }
