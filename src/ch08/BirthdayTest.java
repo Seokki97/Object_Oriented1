8621513a99cc84151299193ab3da720512681c90
programmers1997 <skaksdl1238@gmail.com>
2022-03-12 15:14:35 +0900

    디렉토리 옮기기

---
+++ src/ch08/BirthdayTest.java
@@ -0,0 +1,11 @@
+package ch08;
+
+public class BirthdayTest {
+    public static void main(String[] args) {
+        BrithDay date = new BrithDay();
+        date.setYear(2021);
+        date.setMonth(13);
+        date.setDay(31);
+        date.showDate();
+    }
+}
--- src/ch08/BrithDay.java
+++ src/ch08/BrithDay.java
@@ -4,4 +4,58 @@ public class BrithDay {
     private int day;
     private int month;
     private int year;
+
+    private boolean isVaild; //boolean은 초기화해주지 않으면 자동으로 false
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day){
+        if(day<1 || day >31){
+            isVaild = false;
+        }
+        else {
+            isVaild = true;
+            this.day = day;
+        }
+
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month){
+        if(month<1 || month >12){
+            isVaild = false;
+        }
+        else {
+            isVaild = true;
+            this.month = month;
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year){
+        if(year<1 || year >9999){
+            isVaild = false;
+        }
+        else {
+            isVaild = true;
+            this.year = year;
+        }
+    }
+
+    public void showDate(){
+        if(isVaild == true) {
+            System.out.println( this.year + "년" + this.month + "월" + this.day + "일 입니다.");
+        }
+        else{
+            System.out.println("유효하지 않은 날짜입니다.");
+        }
+    }
 }
