9ddc6bd9239799d7f16644470012db107ac6fae0
programmers1997 <skaksdl1238@gmail.com>
2022-03-12 13:57:09 +0900

    commit

--- src/ch07/Execute.java
+++ src/ch07/Execute.java
@@ -3,6 +3,17 @@ package ch07;
 public class Execute {
 
     public static void main(String[] args) {
+    Student studentLee = new Student(100, "lee");
+    studentLee.setKoreaSubject("국어", 100);
+    studentLee.setMathSubject("수학", 100);
 
+
+    Student studentPark = new Student(100, "lee");
+
+    studentPark.setKoreaSubject("국어", 100);
+    studentPark.setMathSubject("수학", 100);
+
+    studentLee.showScoreInfo();
+    studentPark.showScoreInfo();
     }
 }
