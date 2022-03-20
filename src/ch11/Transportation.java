41cca7c8cdeb493337bbd01bae5d1dcf8c4749e2
programmers1997 <skaksdl1238@gmail.com>
2022-03-20 17:33:33 +0900

    예제

--- src/ch11/Transportation.java
+++ src/ch11/Transportation.java
@@ -17,7 +17,61 @@ public class Transportation {
         System.out.println(this.transformation+this.price);
     }
 
-    //강의 자료
+    /*강의 자료
+   String studentName;
+    int money;
+
+    public Student(String studentName,int money){
+        this.studentName = studentName;
+        this.money = money;
+    }
+
+    public void takeBus(Bus bus){
+        bus.take(1000);
+        this.money -= 1000;
+    }
+    public void takeSubway(Subway subway){
+        subway.take(1200);
+        this.money -= 1200;
+    }
+
+    public void showInfo(){
+        System.out.println("~~~");
+    }
+
+    public class Bus{
+        int busNumber;
+        int passengerCount;
+        int money;
+
+        public Bus(int busNumber){
+            this.busNumber = busNumber;
+        }
+        public void take(int money){
+            this.money += money;
+            passengerCount++;
+        }
+    }
+    public void showBusinfo(){
+        //~~~
+    }
+    public class Subway{
+        int subwayNumber;
+        int passengerCount;
+        int money;
+
+        public Subway(int subwayNumber){
+            this.subwayNumber = subwayNumber;
+        }
+        public void take(int money){
+            this.money +=money;
+            passengerCount++;
+        }
+
+        public void showSubwayInfo(){
+            //~~
+        }
+        */
+    }
 
 
-}
