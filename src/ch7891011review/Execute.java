d76adb992646059b037b04f16173e13b7de15a58
programmers1997 <skaksdl1238@gmail.com>
2022-03-21 18:31:25 +0900

    예제

--- src/ch7891011review/Execute.java
+++ src/ch7891011review/Execute.java
@@ -2,7 +2,12 @@ package ch7891011review;
 
 public class Execute {
     public static void main(String[] args) {
+    Person edward = new Person(20000, "edward");
+    Taxi Taxi1 = new Taxi( "조은택시",10000);
 
+    edward.takeTaxi(Taxi1);
+    edward.showPersonInfo();
+    Taxi1.showInfoTaxi();
 
     }
 }
