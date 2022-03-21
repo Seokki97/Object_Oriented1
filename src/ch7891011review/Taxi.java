68ca6fb4a2dfc213532c25fe424a48a596c97736
programmers1997 <skaksdl1238@gmail.com>
2022-03-21 18:30:41 +0900

    예제

--- src/ch7891011review/Taxi.java
+++ src/ch7891011review/Taxi.java
@@ -1,4 +1,20 @@
 package ch7891011review;
 
 public class Taxi {
+    int price;
+    String taxiName;
+    int income;
+    int count;
+    public Taxi( String taxiName,int price){
+        this.taxiName = taxiName;
+        this.price = price;
+    }
+    public void takeTaxi(int income){
+        this.income += income;
+        count++;
+    }
+
+    public void showInfoTaxi(){
+        System.out.println(taxiName+"의 승객은" + count +"이고 수입은" +income+"입니다.");
+    }
 }
