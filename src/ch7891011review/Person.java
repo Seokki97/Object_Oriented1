1969d2f89e749bc5a6b1ffa9389865cb2662b10a
programmers1997 <skaksdl1238@gmail.com>
2022-03-21 18:31:18 +0900

    예제

--- src/ch7891011review/Person.java
+++ src/ch7891011review/Person.java
@@ -1,4 +1,20 @@
 package ch7891011review;
 
 public class Person {
+    int haveMoney;
+    String name;
+
+    public Person(int haveMoney, String name){
+        this.haveMoney = haveMoney;
+        this.name = name;
+    }
+
+    public void takeTaxi(Taxi taxi){
+        taxi.takeTaxi(10000);
+        haveMoney -= taxi.price;
+    }
+
+    public void showPersonInfo(){
+        System.out.println(this.name+"의 남은 돈은" + haveMoney+"입니다.");
+    }
 }
