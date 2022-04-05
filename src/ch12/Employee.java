e4a375f8f737d6d5d679117904798b945e6bc6f8
programmers1997 <skaksdl1238@gmail.com>
2022-04-05 16:09:31 +0900

    예제

--- src/ch12/Employee.java
+++ src/ch12/Employee.java
@@ -2,4 +2,34 @@ package ch12;
 
 public class Employee {
 
+    static int serialNum = 1000;
+
+    private int employeeId;
+    private String employeeName;
+    private String department;
+
+    public int getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName(){
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName){
+        this.employeeName = employeeName;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
 }
--- src/ch12/Execute.java
+++ src/ch12/Execute.java
@@ -2,6 +2,16 @@ package ch12;
 
 public class Execute {
     public static void main(String[] args) {
+        Employee c1 = new Employee();
+
+        c1.setEmployeeId(c1.serialNum);
+        c1.setEmployeeName("Junseok");
+        c1.setDepartment("개발자");
+
+        System.out.println(c1.getEmployeeName());
+        System.out.println(c1.getDepartment());
+        System.out.println(c1.getEmployeeId());
+
 
     }
 }
