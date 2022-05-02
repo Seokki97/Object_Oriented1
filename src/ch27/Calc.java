1411d87013f1da386e8dfafd5a7818625c878f16
programmers1997 <skaksdl1238@gmail.com>
2022-05-02 19:31:30 +0900

    인터페이스의 여러가지 요소

---
+++ src/ch27/Calc.java
@@ -0,0 +1,26 @@
+package ch27;
+
+public interface Calc {
+
+    double PI =3.14;
+    int ERROR = -999999;
+
+    int add(int num1, int num2);
+    int substract(int num1, int num2);
+    int times(int num1, int num2);
+    int divide(int num1, int num2);
+
+    //디폴트 예제
+    default void description(){
+        System.out.println("정수의 사칙연산을 제공합니다");
+    }
+//정적메서드 예제
+    static int total(int[] arr){
+        int total = 0;
+        for(int num :arr){
+            total += num;
+        }
+        return total;
+    }
+
+}
---
+++ src/ch27/CalculatorTest.java
@@ -0,0 +1,19 @@
+package ch27;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        Calc calc = new CompleteCalc();
+
+       System.out.println(calc.add(3,4));
+        System.out.println(calc.substract(4,6));
+        System.out.println(calc.times(3,4));
+        System.out.println(calc.divide(10,2));
+
+        //타입을 Calc로 했기 떄문에 인터페이스에 있는 메서드만 쓸수있음 그래서 showinfo가 안됨
+        //calc.showInfo();
+         calc.description();
+         int[] arr = {1,2,3,4};
+         System.out.println(Calc.total(arr));
+    }
+}
