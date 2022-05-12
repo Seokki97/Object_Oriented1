ac2062da7794c1d92cb54fc50ea4c6b16ad2821d
programmers1997 <skaksdl1238@gmail.com>
2022-05-12 23:16:39 +0900

    commit

--- Chapter5/src/ch04/MyArrayStack.java
+++ Chapter5/src/ch04/MyArrayStack.java
@@ -1,4 +1,46 @@
 package ch04;
 
+import ch02.MyArray;
+
 public class MyArrayStack {
+
+    int top;
+    MyArray arrayStack;
+
+    public MyArrayStack(){
+    top = 0;
+    this.arrayStack = new MyArray();
+    }
+
+    public MyArrayStack(int size){
+        arrayStack = new MyArray(size);
+    }
+
+    public void push(int data){
+
+    }
+
+    public int pop(){
+
+    }
+
+    public int peek(){
+
+    }
+
+    public int getSize(){
+
+    }
+
+    public boolean isFull(){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public void printAll(){
+        arrayStack.printAll();
+    }
 }
