509f8821c03d09cc708af3592b4042244cfd08a7
programmers1997 <skaksdl1238@gmail.com>
2022-05-13 17:01:10 +0900

    commit

---
+++ Chapter5/src/ch06/ThreeDPrinter3.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class ThreeDPrinter3 {
+
+    private Object material;
+
+    public void setMaterial(Powder material){
+        this.material = material;
+    }
+
+    public Object getMaterial(){
+        return material;
+    }
+
+
+}
