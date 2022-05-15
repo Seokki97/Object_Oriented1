4678c58df22c31ef0af5fb7516c85cdbf3ae278d
programmers1997 <skaksdl1238@gmail.com>
2022-05-15 12:42:48 +0900

    제네릭

---
+++ Chapter5/src/ch06/GenericPrinterTest.java
@@ -0,0 +1,14 @@
+package ch06;
+
+public class GenericPrinterTest {
+    public static void main(String[] args) {
+        Powder powder = new Powder();
+        GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+        //<> 다이아몬드 컬렉션
+
+        powderPrinter.setMaterial(powder);
+
+        Powder p = powderPrinter.getMaterial();
+        System.out.println(powderPrinter.toString());
+    }
+}
