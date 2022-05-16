c95c83f6cb4d322e736ce294d576e2b8dea60236
programmers1997 <skaksdl1238@gmail.com>
2022-05-16 15:37:24 +0900

    commit

--- Chapter5/src/ch07/Powder.java
+++ Chapter5/src/ch07/Powder.java
@@ -1,8 +1,11 @@
 package ch07;
 
-public class Powder {
+public class Powder extends Material {
 
     public String toString(){
         return "재료는 파우더입니다";
     }
+
+    @Override
+    public void doPrinting(){};
 }
