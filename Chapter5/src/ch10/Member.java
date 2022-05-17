9eb6265661fe8976cfef543bb1de446a2acbc566
programmers1997 <skaksdl1238@gmail.com>
2022-05-17 13:08:07 +0900

    연습

--- Chapter5/src/ch10/Member.java
+++ Chapter5/src/ch10/Member.java
@@ -1,4 +1,29 @@
 package ch10;
 
 public class Member {
+    private int memberId;
+    private String memberName;
+
+    public Member(int memberId, String memberName){
+        this.memberId = memberId;
+        this.memberName = memberName;
+
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
 }
--- Chapter5/src/ch10/MemberArrayList.java
+++ Chapter5/src/ch10/MemberArrayList.java
@@ -1,4 +1,7 @@
 package ch10;
 
+import java.util.ArrayList;
+
 public class MemberArrayList {
+    ArrayList<String> arrayList ;
 }
