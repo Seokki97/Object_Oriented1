1ae7b57fd7fb3f24f660f66481216ab6c49545e7
programmers1997 <skaksdl1238@gmail.com>
2022-05-18 20:52:26 +0900

    commit

---
+++ Chapter5/src/ch13/Member.java
@@ -0,0 +1,57 @@
+package ch13;
+
+public class Member {
+
+    private String memberName;
+    private int memberId;
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public Member(int memberId, String memberName) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+
+    }
+
+
+    //이게 중복을 정의해주는 equals메서드를 재정의해준거
+    @Override
+    public int hashCode() {
+        return memberId;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj; //obj를 member로 다운캐스팅
+            if (this.memberId == member.memberId)
+                return true;
+             else
+                return false;
+
+
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return memberId + memberName;
+    }
+
+}
---
+++ Chapter5/src/ch13/MemberTreeSet.java
@@ -0,0 +1,24 @@
+package ch13;
+
+import java.util.TreeSet;
+
+public class MemberTreeSet {
+
+    private TreeSet<Member> treeSet ;
+
+    public MemberTreeSet(){
+        treeSet = new TreeSet<Member>();
+    }
+
+    public void addMember(Member member){
+        treeSet.add(member);
+    };
+
+    public void showAllMember(){
+        for(Member member : treeSet){
+            System.out.println(member);
+        }
+        System.out.println();
+    };
+
+}
