3a57ac535f5aed08859c9406519362e675fb5332
programmers1997 <skaksdl1238@gmail.com>
2022-05-24 13:42:47 +0900

    practice

--- Chapter6/src/ch06/TravelCustomer.java
+++ Chapter6/src/ch06/TravelCustomer.java
@@ -1,4 +1,38 @@
 package ch06;
 
 public class TravelCustomer {
+    //이름 나이 비용
+    private String customerName;    private int customerAge;
+    private int cost;
+
+    public TravelCustomer(String customerName, int customerAge, int cost){
+        this.customerName = customerName;
+        this.customerAge = customerAge;
+        this.cost = cost;
+    }
+
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(int customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
--- Chapter6/src/ch06/TravelTest.java
+++ Chapter6/src/ch06/TravelTest.java
@@ -1,7 +1,31 @@
 package ch06;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TravelTest {
     public static void main(String[] args) {
+
+        TravelCustomer customerLee = new TravelCustomer("이시영", 40, 100);
+        TravelCustomer customerKim = new TravelCustomer("김준석", 26, 100);
+        TravelCustomer customerHong = new TravelCustomer("홍기준", 15, 50);
+
+
+        List<TravelCustomer> customerList = new ArrayList<>();
+
+        customerList.add(customerLee);
+        customerList.add(customerKim);
+        customerList.add(customerHong);
+
+        System.out.println("==고객 명단 추가된 순서대로 출력 ==");
+        customerList.stream().map(c ->c.getCustomerName()).forEach(s->System.out.println(s));
+
+        int total = customerList.stream().mapToInt(c->c.getCost()).sum();
+        System.out.println(" 총 여행 비용은:" +total + "입니다.");
+
+
+        System.out.println("==20세 이상 고객 명단 정렬하여 출력 ==");
+        customerList.stream().filter( s -> s.getCustomerAge() >=20).map(s->s.getCustomerName()).sorted().forEach(s ->System.out.println(s));
 
     }
 }
