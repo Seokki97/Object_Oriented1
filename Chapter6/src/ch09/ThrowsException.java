174b181cc3e0a83a7500a3f3ff8310403453b341
programmers1997 <skaksdl1238@gmail.com>
2022-05-26 17:48:05 +0900

    예외처리

--- Chapter6/src/ch09/ThrowsException.java
+++ Chapter6/src/ch09/ThrowsException.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 public class ThrowsException {
     public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException{
         FileInputStream fis = new FileInputStream(fileName);
-        Class c = Class.forName(className);
+        Class c = Class.forName(className); //여기서 exception이 발생하지만 이걸 미루고 이 메서드를 쓰는 쪽에서 핸들링을 하겠단 말임
         return c;
     }
     public static void main(String[] args) {
@@ -16,11 +16,12 @@ public class ThrowsException {
             test.loadClass("a.txty","java.lang.String");
 
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println(e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }catch (Exception e){
-            e.printStackTrace();
+            System.out.println(e);
+        }catch (Exception e){ //어떤 Exception이 발생할 지 모를때 사용하는것. 디폴트 Exception이라 함
+            // 즉 모든 Exception의 최상위 클래스
+            System.out.println(e);
         }
     }
 
