7fd01e1eca859010d185e2eab42b5b70aea39316
programmers1997 <skaksdl1238@gmail.com>
2022-05-27 01:00:18 +0900

    문자열계산기

--- 문자열계산기/src/StringCalculator.java
+++ 문자열계산기/src/StringCalculator.java
@@ -31,18 +31,41 @@ public class StringCalculator {
     public int calculator(int front , String middle, int back) {
         int result=0;
         if (middle == "+") return result =add(front, back);
+
         else if (middle == "-") return result = subtract(front, back);
+
         else if (middle == "*") return result = multiply(front, back);
+
         else if (middle == "/") return result =divide(front, back);
-        else try{ System.out.println("입력값이 잘못되었습니다");} catch (Exception e){
+
+        else
+            try{ System.out.println("입력값이 잘못되었습니다");
+            }catch (Exception e){
             System.out.println(e);
             }
         return result;
     }
 
+    public String[] splitString(String str){
+        return str.split("");
+    }
+
+    public int toInt(String str){
+        return Integer.parseInt(str);
+    }
+
+    public int calcString(String[] str){
+        int result = toInt(str[0]);
+        //4+6*3+2 -> 10*3+2  6이다. -> 10
+        for(int i =0; i< str.length-2; i+=2){
+            result = calculator(result, String.valueOf(str[i+1].charAt(0)), toInt(str[i+2]));
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-
-        System.out.println(sc.calculator(4,"f",3));
+        String[] str ={"4","+","9"};
+        System.out.println(sc.calcString(str));
     }
 }
