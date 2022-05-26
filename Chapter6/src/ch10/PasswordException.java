3e2cc52065777714c7c8125cbd7823deea50e301
programmers1997 <skaksdl1238@gmail.com>
2022-05-26 19:53:59 +0900

    예외처리 예제

--- Chapter6/src/ch10/PasswordException.java
+++ Chapter6/src/ch10/PasswordException.java
@@ -1,5 +1,10 @@
 package ch10;
 
-public class PasswordException extends IllegalArgumentException{
+public class PasswordException extends IllegalArgumentException{//argument의 오류가 있을때 런타임오류발생하는 exception
+
+    public PasswordException(String message){
+        super(message);
+    }
+
 
 }
---
+++ Chapter6/src/ch10/PasswordTest.java
@@ -0,0 +1,46 @@
+package ch10;
+
+public class PasswordTest {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        if(password ==null){
+        System.out.println("null 오류입니다.");
+        }
+        else if(password.length() < 5  ){
+        System.out.println("문자열 길이 오류입니다");
+        }
+
+        else if(password.matches("[a-zA-Z]+")){
+            System.out.println("하나이상의 숫자나 특수문자가 들어가야합니다.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PasswordTest pt= new PasswordTest();
+        pt.password = null;
+
+        try{
+            pt.setPassword(null);
+        }catch(PasswordException e){
+            System.out.println(e);
+        }
+        try{
+            pt.setPassword("ds2");
+        }catch(PasswordException e){
+            System.out.println(e);
+        }
+        try{
+            pt.setPassword("jasdop");
+        }catch (PasswordException e){
+            System.out.println(e);
+        }
+
+
+    }
+}
