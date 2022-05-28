492196b85ee46e304972bcc0ebe3f99f7a64bf42
programmers1997 <skaksdl1238@gmail.com>
2022-05-28 14:02:03 +0900

    baseballgame

--- BaseballGame/src/baseballgame/Computer.java
+++ BaseballGame/src/baseballgame/Computer.java
@@ -1,4 +1,24 @@
 package baseballgame;
 
+import java.util.Random;
+
 public class Computer {
+
+    private int[] setting = new int[3];
+
+    public int[] getSetting() {
+        return setting;
+    }
+
+    public void setSetting(int[] setting) {
+        this.setting = setting;
+    }
+
+    public int[] randomBob(){
+        Random rand = new Random();
+        for(int i = 0 ; i< 3; i++){
+         setting[i] = rand.nextInt(9);
+        }
+    return setting;
+    }
 }
