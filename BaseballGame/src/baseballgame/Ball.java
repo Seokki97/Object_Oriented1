204c47dd248a444bc3e1543406f3dba173648204
programmers1997 <skaksdl1238@gmail.com>
2022-05-30 14:46:40 +0900

    refactoring - 리팩토링

--- BaseballGame/src/baseballgame/Ball.java
+++ BaseballGame/src/baseballgame/Ball.java
@@ -1,5 +1,7 @@
 package baseballgame;
 
+import java.util.List;
+
 public class Ball {
     final int INITIAL_NUM = -1;
     static final int SIZE = 3;
@@ -11,25 +13,23 @@ public class Ball {
         변경해봅시다. 또한, 객체의 캡슐화란 무엇일까요?
      */
 
-    public String makeStrike(int[] playerNum, int[] computerNum) {
+    public int makeStrike(Player player, Computer computer) {
         strike = 0;
         for (int i = 0; i < 3; i++) {
-            if (playerNum[i] == computerNum[i]) {
+            if (player.getPerson().get(i) == computer.getComputerAnswerValue().get(i)) {
                 strike++;
-                playerNum[i] = INITIAL_NUM;
+                player.getPerson().add(i,INITIAL_NUM);
             }
         }
-        return strike + "스트라이크";
+        return strike;
     }
     /*
         for문의 depth가 3이예요. 변경해봅시다.
         또한 매직넘버를 제거해봅시다.
         그리고 현재 구조에서 Ball 객체는 스트라이크와 볼의 개수를 판단하는 역할을 하는 것으로 보이는데,
-        return 문에서는 strike + "스트라이크"를 리턴해줌으로써 출력형식을 지정해주고있죠.
-        출력형식을 지정해주는 것이 Ball 객체의 역할이 맞을까요?
-     */
+       */
 
-    public String makeBall(int[] playerNum, int[] computerNum) {
+    public int makeBall(int[] playerNum, int[] computerNum) {
         ball = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -38,15 +38,14 @@ public class Ball {
                 }
             }
         }
-        return ball + "볼";
+        return ball ;
     }
 
     /*
         for문의 depth가 4예요. 변경해봅시다.
         또한 매직넘버를 제거해봅시다.
         그리고 현재 구조에서 Ball 객체는 스트라이크와 볼의 개수를 판단하는 역할을 하는 것으로 보이는데,
-        return 문에서는 strike + "스트라이크"를 리턴해줌으로써 출력형식을 지정해주고있죠.
-        출력형식을 지정해주는 것이 Ball 객체의 역할이 맞을까요?
+
      */
 
     public int getStrike() { // 일반적으로 getter는 클래스의 제일 하단에 작성해요. (오버라이드한 메서드보다는 상단에 작성합니다!)
