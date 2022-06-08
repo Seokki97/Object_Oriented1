a7d15f23f217ea46d686ba3632c701721a6e6c66
programmers1997 <skaksdl1238@gmail.com>
2022-06-08 17:46:15 +0900

    commit

--- BaseballGame/src/baseballgame2/GameMessage.java
+++ BaseballGame/src/baseballgame2/GameMessage.java
@@ -1,19 +1,23 @@
 package baseballgame2;
 
 public class GameMessage {
-    public static void showStrikeScore(int strike){
-        System.out.print(strike +"스트라이크 ");
+    public static void showStrikeScore(int strike) {
+        System.out.print(strike + "스트라이크 ");
     }
-    public static void showballScore(long ball){
+
+    public static void showBallScore(long ball) {
         System.out.println(ball + "볼");
     }
-    public static void showNothing(){
+
+    public static void showNothing() {
         System.out.println("낫싱");
     }
-    public static void  showRetryMessage(){
+
+    public static void showRetryMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-    public static void showEndGameMessage(){
+
+    public static void showEndGameMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
