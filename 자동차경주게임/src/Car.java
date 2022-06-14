6d9cd91d445311813d411c4215814503aeb25074
programmers1997 <skaksdl1238@gmail.com>
2022-06-14 12:16:12 +0900

    자동차경주게임

---
+++ 자동차경주게임/src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private final String carName;
+    private  String carPosition;
+
+    public Car(String carName, String carPosition) {
+        this.carName = carName;
+        this.carPosition = carPosition;
+    }
+
+    public void move(FowardCondition fowardCondition){
+        if(fowardCondition.fowardCarCondition()){
+            carPosition += "-";
+        }
+    }
+
+
+
+}
