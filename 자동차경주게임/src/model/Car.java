ea295e31c16cc7a080b7252dec8217e9455ad569
JunSeok Kim <skaksdl1238@gmail.com>
2022-09-12 21:24:08 +0900

    운영체제란

--- 자동차경주게임/src/model/Car.java
+++ 자동차경주게임/src/model/Car.java
@@ -4,20 +4,16 @@ public class Car {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String ERROR_INVAILD_NAME_LENGTH = "[ERROR] 자동차 이름은 5글자 이하로 설정해 주세요";
     private String carName;
+    private int carPosition;
 
-    public Car(String carName){
+    public Car(String carName, int carPosition){
         this.carName = carName;
-    }
-
-    public String getCarName() {
-        return carName;
+        this.carPosition = carPosition;
     }
 
     @Override
     public String toString() {
-        return "Car{" +
-                "carName='" + carName + '\'' +
-                '}';
+        return carName +":" + '\'' + carPosition ;
     }
 
     private void vaildateNamingLength(String carname){
