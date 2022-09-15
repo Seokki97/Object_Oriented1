8b065db076e8f03f916a997df106e854d232f3a6
JunSeok Kim <skaksdl1238@gmail.com>
2022-09-15 17:33:17 +0900

    software engineering

--- hellospring/src/main/java/controller/ApiController.java
+++ hellospring/src/main/java/controller/ApiController.java
@@ -1,16 +1,22 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import controller.dto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    //text를 내려주는것
     @GetMapping("/text")
     public String text(@RequestParam String account){
         return account;
+    }
+
+    //JSON을 내려주는것
+    @PostMapping
+    public User json(@RequestBody User user){
+        return user;
+
     }
 }
